9bbb252a0306c63495cb4c33db9f3f561f0556ef
Vladislav Kalambet <vlad.kalambet@yandex.ru>
2024-04-27 08:07:31 +0000

    init

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    rand_nums = []
+    for i in range(10):
+        rand_nums.append(random.randint(0,100))
+    return render_template("index.html", a=rand_nums)
